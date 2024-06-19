fdefacdf16c7b445296d9c60afa07096dca659ce
Ben <bslowinski02@gmail.com>
2024-06-19 17:14:56 -0500

    laid out the beginning of routing

--- src/components/applayout/app-layout.component.jsx
+++ src/components/applayout/app-layout.component.jsx
@@ -2,13 +2,12 @@ import { Outlet } from "react-router-dom";
 import Navigation from "../../Routes/navigation/navigation.component";
 
 function AppLayout() {
-    return (
-        <div className="layout">
-            <Navigation />
-            <Outlet />
-
-        </div>
-      );
+  return (
+    <>
+      <Navigation />
+      <Outlet />
+    </>
+  );
 }
 
-export default AppLayout ;
+export default AppLayout;
